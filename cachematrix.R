4945393e0aa3f33c5d976c95449a8953a56fb5d2
Ivy-YYT <Ivy.yangyuting@gmail.com>
2019-05-16 18:53:25 +0800

    revised

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+#makeCacheMatrix creates a function that store the matrix inversion. 
+#cacheSolve is used to check whether a matrix inversion is the stored or not. 
+#If not, the new matrix inversion will be stored.
+
+
 ## Write a short comment describing this function
+#makeCacheMatrix is a function that contains four functions inside. When a matrix is applied, the printed results is a list of the four functions.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -18,6 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+# Apply the results of above printed list here, and the cacheSolve function will first check if the matrix is stored. If yes, the codes from "data <- x$get()"
+# will not be executed; if no, the matrix will be inversed.
+
+
 cacheSolve <- function(x, ...) {
         inv <- x$getinversion()
         if(!is.null(inv)) {
@@ -29,4 +39,13 @@ cacheSolve <- function(x, ...) {
         x$setinversion(inv)
         inv
         ## Return a matrix that is the inverse of 'x'
+        
+        # check functions:
+        # > t <- makeCacheMatrix(matrix(c(2, 4, 7, 12, 23, 45, 2, 9, 12), 3, 3))
+        # > cacheSolve(t)
+        # [,1]  [,2]  [,3]
+        # [1,]  3.225  1.35 -1.55
+        # [2,] -0.375 -0.25  0.25
+        # [3,] -0.475  0.15  0.05
+        
 }
